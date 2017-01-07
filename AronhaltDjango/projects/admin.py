5f1d93a23aef3b86d5f8fcb58437adfd11c3c021
aronhalt <aronhaltb@gmail.com>
2017-01-07 00:55:49 -0800

    installed node.js, restframework, gulp, bower, react and jquery

--- AronhaltDjango/projects/admin.py
+++ AronhaltDjango/projects/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Project
 # Register your models here.
+class projectAdmin(admin.ModelAdmin):
+	list_display = ('project_title',)
+	search_fields = ['project_title']
 
-admin.site.register(Project)
+admin.site.register(Project, projectAdmin)
--- AronhaltDjango/projects/urls.py
+++ AronhaltDjango/projects/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<project_id>[0-9]+/$', views.detail, name='detail'),
+	url(r'^(?P<project_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^api/(?P<pk>[0-9]+)/$', views.projectDetail.as_view(), name='projectDetail'),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
